a827961d4b73ea403c9abc7b3d2ba919b0acb6a6
jawolf17 <jawolf17@gmail.com>
2020-09-07 21:08:25 -0400

    Added function to save temperature data in the temperature table. * Function takes in timestamp, thermometer number, and temperature reading. * Function established db connection and saves data

--- sensor_db/sql_db.py
+++ sensor_db/sql_db.py
@@ -5,20 +5,16 @@ class SQLDB:
     """Initalize SQLDB class with DB name"""
     def __init__(self):
         self.__db_name = "sensor_data.db"
-        self.__connection = None
 
         self.__create_sensor_tables()
 
-    """Connects to SQLite DB and returns a cursor"""
+    """Connects to SQLite DB and returns a connection"""
     def __connect(self):
-            self.__connection = sqlite3.connect(self.__db_name)
-            return self.__connection.cursor()
+            return sqlite3.connect(self.__db_name)
 
     """Closes the connection currently open"""
-    def __close(self):
-        if self.__connection != None: 
-            self.__connection.close()
-            self.__connection = None
+    def __close(self, connection):
+           connection.close()
 
     """ Creates Tables required for storing sensor data"""
     def __create_sensor_tables(self):
@@ -27,9 +23,9 @@ class SQLDB:
             # Try to establist a connection and make tables
 
             # Create Thermomether Table
-            cursor = self.__connect()
-            cursor.execute("CREATE TABLE IF NOT EXISTS temperature (timestamp datetime2 PRIMARY KEY, thermometer_number int, temperature float);")
-            self.__connection.commit()
+            connection = self.__connect()
+            connection.cursor().execute("CREATE TABLE IF NOT EXISTS temperature (timestamp datetime2 PRIMARY KEY, thermometer_number int, temperature float)")
+            connection.commit()
 
         except Exception as error:
             # Print error is one occurs.
@@ -37,12 +33,27 @@ class SQLDB:
 
         finally:
             # Always close the connection when complete.
-            self.__close()
+            self.__close(connection)
 
-    
+    """ Stores a temperature reading with the given data"""
+    def store_temperature_data(self, timestamp, therm_num, temp):
 
-    
-        
+        try:
+            # Get connetion to db
+            connection = self.__connect()
 
-    
+            # Set-up string for insertion
+            sql_statement = "INSERT INTO temperature VALUES ({0},{1},{2});"
+            sql_statement = sql_statement.format(timestamp, therm_num, temp)
 
+            # Execute statement
+            connection.cursor().execute(sql_statement)
+            connection.commit()
+
+        except Exception as error:
+            # Print error in case of exception
+            print(error)
+
+        finally:
+            # Always close the connection
+            connection.close()
